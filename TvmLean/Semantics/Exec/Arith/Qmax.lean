9b5b669872323facf1a22d7f4839209514ce9d25
Gusarich <danielsedovzzz@gmail.com>
2026-02-08 20:39:46 +0300

    feat: QMAX

--- TvmLean/Semantics/Exec/Arith/Qmax.lean
+++ TvmLean/Semantics/Exec/Arith/Qmax.lean
@@ -6,6 +6,7 @@ set_option maxHeartbeats 1000000 in
 def execInstrArithQmax (i : Instr) (next : VM Unit) : VM Unit := do
   match i with
   | .qmax =>
+      VM.checkUnderflow 2
       let x ← VM.popInt
       let y ← VM.popInt
       match x, y with
