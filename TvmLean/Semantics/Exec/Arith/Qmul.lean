ff8ecf8d557ab5e4ba62ff431c80325cc9161c28
Gusarich <danielsedovzzz@gmail.com>
2026-02-08 18:50:56 +0300

    feat: QMUL

--- TvmLean/Semantics/Exec/Arith/Qmul.lean
+++ TvmLean/Semantics/Exec/Arith/Qmul.lean
@@ -6,6 +6,7 @@ set_option maxHeartbeats 1000000 in
 def execInstrArithQmul (i : Instr) (next : VM Unit) : VM Unit := do
   match i with
   | .qmul =>
+      VM.checkUnderflow 2
       let y ← VM.popInt
       let x ← VM.popInt
       VM.pushIntQuiet (x.mul y) true
