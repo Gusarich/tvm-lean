39e7570f3c8f21adf6d1c30570eca3150266d9b1
Gusarich <danielsedovzzz@gmail.com>
2026-02-08 20:39:59 +0300

    feat: QSUBR

--- TvmLean/Semantics/Exec/Arith/Qsubr.lean
+++ TvmLean/Semantics/Exec/Arith/Qsubr.lean
@@ -6,6 +6,7 @@ set_option maxHeartbeats 1000000 in
 def execInstrArithQsubr (i : Instr) (next : VM Unit) : VM Unit := do
   match i with
   | .qsubr =>
+      VM.checkUnderflow 2
       let y ← VM.popInt
       let x ← VM.popInt
       VM.pushIntQuiet (y.sub x) true
